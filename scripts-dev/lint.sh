452932988c204ce5cbeb45d09492bb8cfdeeb4ba
Andrew Morgan <andrew@amorgan.xyz>
2022-01-09 18:55:26 +0000

    Add a linting script to check code locally
    Simply execute './scripts-dev/lint.sh' before each commit in order to
    automatically fix, or point out, any linting errors in your code.

---
+++ scripts-dev/lint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Runs linting scripts over the local checkout. Can be run as:
+# ./lint.sh
+# to lint all project files, or as:
+# ./lint.sh file.py directory file2.py
+# to lint specific files and directories.
+#
+# List of commands that are run:
+# isort - sorts import statements
+# flake8 - lints and finds mistakes
+# black - opinionated code formatter
+
+set -e
+
+if [ $# -ge 1 ]
+then
+    files=$*
+  else
+    files="main.py"
+fi
+
+echo "Linting these locations: $files"
+isort $files
+flake8 $files
+python3 -m black $files
