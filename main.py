67bc178a599bb53405484cf3e23a74c874d4b8b9
Jackson Abascal <jacksonabascal@gmail.com>
2022-01-18 17:06:24 -0500

    separate and capitalize constants

--- main.py
+++ main.py
@@ -34,10 +34,6 @@ seconds_between_songs = int(os.environ.get("BUSTY_COOLDOWN_SECS", 10))
 attachment_directory_filepath = os.environ.get("BUSTY_ATTACHMENT_DIR", "attachments")
 # The Discord role needed to perform bot commands
 dj_role_name = os.environ.get("BUSTY_DJ_ROLE", "bangermeister")
-# Max number of characters in an embed description (currently 4096 in Discord)
-embed_description_limit = 4096
-# Color of !list embed
-list_embed_color = 0xDD2E44
 
 # GLOBAL VARIABLES
 # The channel to send messages in
@@ -385,10 +381,7 @@ async def command_list(message: Message):
                 break
             except (HTTPException, NotFound) as e:
                 print("Pinning tracklist failed: ", e)
-<<<<<<< HEAD
                 break
-=======
->>>>>>> b360e4a (removed duplicate pin code)
 
     # Update global channel content
     global current_channel_content
