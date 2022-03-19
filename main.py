c028845ba2af10f4d2275462f1d9526b9fc6eb38
Andrew Morgan <andrew@amorgan.xyz>
2022-03-19 12:16:55 +0100

    return types for more functions

--- main.py
+++ main.py
@@ -85,12 +85,12 @@ client = Client(intents=intents)
 
 
 @client.event
-async def on_ready():
+async def on_ready() -> None:
     print("We have logged in as {0.user}.".format(client))
 
 
 @client.event
-async def on_message(message: Message):
+async def on_message(message: Message) -> None:
     if message.author == client.user:
         return
 
