e94868cb740cf3dac4e7d2d4f55516adf2ca7ded
Jackson Abascal <jacksonabascal@gmail.com>
2022-08-21 11:28:36 -0700

    Create BustController (#127)

--- src/voting.py
+++ src/voting.py
@@ -12,7 +12,7 @@ import song_utils
 # internal logic remaining here
 
 
-async def command_form(
+async def generate_form(
     message: Message, google_drive_image_link: Optional[str] = None
 ) -> None:
     # Escape strings so they can be assigned as literals within appscript
