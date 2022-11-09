90e1bf1d0f7ba5e2c57dded4727078d49d3d4934
Jackson Abascal <jacksonabascal@gmail.com>
2022-11-08 18:20:49 -0800

    make loaded image persistent across reboots

---
+++ src/persistent.py
@@ -0,0 +1,36 @@
+import contextlib
+import os
+from typing import Optional
+
+
+class PersistentString:
+    """A string wrapper which is persistent across reboots.
+
+    Methods:
+        get(value):
+            Gets the value of the string
+        set(value):
+            Sets the value of the string and writes to disk
+    """
+
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+        self._value: Optional[str] = None
+        # Load string if it exists
+        with contextlib.suppress(FileNotFoundError):
+            with open(self.filepath) as f:
+                self._value = f.read()
+
+    def set(self, value: Optional[str]):
+        self._value = value
+
+        # Either save string, or delete state file if None
+        if self._value is None:
+            with open(self.filepath, "w") as f:
+                f.write(self._value)
+        else:
+            with contextlib.suppress(FileNotFoundError):
+                os.remove(self.filepath)
+
+    def get(self) -> Optional[str]:
+        return self._value
