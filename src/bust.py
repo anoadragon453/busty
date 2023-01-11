426ee5ff8df58a99bb9fb6e51b467de6e1d6e2ac
Jackson Abascal <jacksonabascal@gmail.com>
2023-01-11 10:24:28 -0800

    Handle song length errors without crashing (#154)

--- src/bust.py
+++ src/bust.py
@@ -316,11 +316,18 @@ class BustController:
 
         # Compute map of submitter --> total length of all submissions
         submitter_to_len = defaultdict(lambda: 0.0)
+        # Default entry in case no valid songs w/ length
+        bot_member = interaction.guild.get_member(self.client.user.id)
+        submitter_to_len[bot_member] = 0.0
 
+        errors = False
         for submit_message, attachment, local_filepath in self.bust_content:
             song_len = song_utils.get_song_length(local_filepath)
-            submitter = submit_message.author
-            submitter_to_len[submitter] += song_len
+            if song_len:
+                submitter = submit_message.author
+                submitter_to_len[submitter] += song_len
+            else:
+                errors = True
 
         # User with longest total submission length
         longest_submitter = max(submitter_to_len, key=submitter_to_len.get)
@@ -331,11 +338,17 @@ class BustController:
                 f"*Number of tracks:* {num_songs}",
                 f"*Total track length:* {song_utils.format_time(songs_len)}",
                 f"*Total bust length:* {song_utils.format_time(bust_len)}",
-                f"*Unique submitters:* {len(submitter_to_len)}",
+                # We account for the bot being included in the `submitter_to_len` dict
+                # by subtracting 1 from the total count.
+                f"*Unique submitters:* {len(submitter_to_len)-1}",
                 f"*Longest submitter:* {longest_submitter.mention} - "
                 + f"{song_utils.format_time(longest_submitter_time)}",
             ]
         )
+        if errors:
+            embed_text += (
+                "\n\n**There were some errors. Statistics may not be accurate.**"
+            )
         embed = Embed(
             title="Listed Statistics",
             description=embed_text,
