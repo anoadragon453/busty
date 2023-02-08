f263918137e115182bb97dc528c4c0e7c651d39d
Jack Cogan <jackcogan97@y7mail.com>
2023-02-08 22:31:34 +0000

    Change jump url for previews

--- src/main.py
+++ src/main.py
@@ -267,7 +267,7 @@ async def preview(
             uploaded_file,
             user,
             random_emoji,
-            interaction.channel.jump_url,
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         )
         cover_art = song_utils.get_cover_art(attachment_filepath)
 
