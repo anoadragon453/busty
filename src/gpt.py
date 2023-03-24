35eafaed6e5ebbf041bfb7f1ec5e2509ed85d12a
Jackson Abascal <jacksonabascal@gmail.com>
2023-03-24 15:05:36 -0700

    Types
    Co-authored-by: Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>

--- src/gpt.py
+++ src/gpt.py
@@ -285,7 +285,7 @@ async def get_response_text(message: Message) -> Optional[str]:
     return None
 
 
-async def respond(message: Message):
+async def respond(message: Message) -> None:
     # React with a "stop" hand, we're responding to someone else
     if gpt_lock.locked() or not context_data:
         raised_hand_emoji = "\N{RAISED HAND}\U0001F3FF"
