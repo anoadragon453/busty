541a0c188c1feaaf15606d3f68393258a09e4b2f
Jackson Abascal <jacksonabascal@gmail.com>
2025-12-31 14:01:24 -0800

    Add /say command
    This allows an admin to pretend to be Busty and say something. Similar
    to the announce command, but with more plausible deniability. Also
    change the default perms of announce (and say) to admin-only.

--- src/busty/commands/admin.py
+++ src/busty/commands/admin.py
@@ -1,17 +1,17 @@
 """Admin commands for the Discord bot."""
 
-from discord import Embed, Interaction, TextChannel
+from discord import Embed, Interaction, TextChannel, app_commands
 
 from busty.bot import BustyBot
 from busty.config import constants
-from busty.decorators import guild_only, has_dj_role
+from busty.decorators import guild_only
 
 
 def register_commands(client: BustyBot) -> None:
     """Register admin commands."""
 
     @client.tree.command(name="announce")
-    @has_dj_role()
+    @app_commands.default_permissions(administrator=True)
     @guild_only()
     async def announce(
         interaction: Interaction,
@@ -52,3 +52,38 @@ def register_commands(client: BustyBot) -> None:
         else:
             interaction_reply = f"Announcement has been sent in {channel.mention}."
         await interaction.response.send_message(interaction_reply, ephemeral=True)
+
+    @client.tree.command(name="say")
+    @app_commands.default_permissions(administrator=True)
+    @guild_only()
+    async def say(
+        interaction: Interaction,
+        message: str,
+        channel: TextChannel | None = None,
+    ) -> None:
+        """Send a message as the bot into a channel."""
+        assert interaction.guild_id is not None  # Guaranteed by @guild_only()
+        await interaction.response.defer(ephemeral=True)
+        if channel is None:
+            if not isinstance(interaction.channel, TextChannel):
+                await interaction.followup.send(
+                    "This command can only be used in a text channel.", ephemeral=True
+                )
+                return
+            channel = interaction.channel
+
+        # Disallow sending messages from one guild into another.
+        if channel.guild.id != interaction.guild_id:
+            await interaction.followup.send(
+                "Sending messages to a guild outside of this channel is not allowed.",
+                ephemeral=True,
+            )
+            return
+
+        await channel.send(message)
+
+        if channel.id == interaction.channel_id:
+            interaction_reply = "Message has been sent."
+        else:
+            interaction_reply = f"Message has been sent in {channel.mention}."
+        await interaction.followup.send(interaction_reply, ephemeral=True)
